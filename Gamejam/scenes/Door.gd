4581c633ff84f157dd4af3c30c5ab22a2c449dd2
bionicle564 <57322673+bionicle564@users.noreply.github.com>
2021-06-12 01:28:38 -0400

    doors added

--- Gamejam/scenes/Door.gd
+++ Gamejam/scenes/Door.gd
@@ -4,21 +4,19 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
 onready var animation = $AnimatedSprite
-
-export var doorName = ""
-export var useable = true
+onready var colider = $CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func open():
+	animation.play()
+	colider.disabled = true
+	
 func use():
-	if useable: 
-		print(doorName)
-		animation.play("used")
-		get_tree().call_group(doorName,"open")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
